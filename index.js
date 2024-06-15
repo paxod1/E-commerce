a9d7e93c5785ec986e8fad9862971a35aabda367
chiku binoy <chikubinoybinoy@gmail.com>
2024-06-15 14:32:05 +0530

    hosting

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/company',CompanyRouter)
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'Frontend/app/build', 'index.html'));
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`port ${PORT} is connected`);
 });
