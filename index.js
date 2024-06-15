1ca031868002d7bc617ab445b8206c7c1c9cb7ca
chiku binoy <chikubinoybinoy@gmail.com>
2024-06-15 21:57:59 +0530

    to hosts

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors({
   credentials: true,
 }));
 
-
 app.use(express.static(path.join(__dirname, 'Frontend/app/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
