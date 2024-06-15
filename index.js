a7c3219c0c2f40dec8361bcf1d3ab4b182e4b32d
chiku binoy <chikubinoybinoy@gmail.com>
2024-06-15 21:31:23 +0530

    to host

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const express=require('express')
-const app =express()
-const mongoose=require('mongoose')
-const dontenv=require('dotenv')
-const cors=require('cors')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const path = require('path');
-const bodyParser = require('body-parser'); 
-dontenv.config()
+const bodyParser = require('body-parser');
+
+dotenv.config();
 
 app.use(cors({
-  origin: 'https://e-commerce-100.onrender.com',
+  origin: 'https://e-commerce-102.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
-
 app.use(express.static(path.join(__dirname, 'Frontend/app/build')));
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const UserRouter = require('./Router/UserRouter');
+const AdminRouter = require('./Router/AdminRouter');
+const CompanyRouter = require('./Router/CompanyRouter');
 
-const UserRouter=require('./Router/UserRouter')
-const AdminRouter=require('./Router/AdminRouter')
-const CompanyRouter=require('./Router/CompanyRouter')
+mongoose.connect(process.env.MongoUrl).then(() => {
+  console.log("database is connected");
+}).catch((err) => {
+  console.log(err);
+});
 
-mongoose.connect(process.env.MongoUrl).then(()=>{
-   console.log("data base is connected")
-}).catch((err)=>{
-    console.log(err)
-})
+app.use('/home', UserRouter);
+app.use('/Admin', AdminRouter);
+app.use('/company', CompanyRouter);
 
-
-app.use('/home',UserRouter)
-app.use('/Admin',AdminRouter)
-app.use('/company',CompanyRouter)
-
-app.get('*', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, 'Frontend/app/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`port ${PORT} is connected`);
+  console.log(`port ${PORT} is connected`);
 });
-
-
