7793098820ee09118eb185d4a90f7b04471888b1
paxod1 <156282097+paxod1@users.noreply.github.com>
2024-06-17 14:54:41 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
 const path = require('path');
-const bodyParser = require('body-parser');
 
 dotenv.config();
 
@@ -20,28 +18,29 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, 'Frontend/app/build')));
 app.use(express.json());
 
+// Routers
 const UserRouter = require('./Router/UserRouter');
 const AdminRouter = require('./Router/AdminRouter');
 const CompanyRouter = require('./Router/CompanyRouter');
 
-mongoose.connect(process.env.MongoUrl).then(() => {
-  console.log("data base is connected");
-}).catch((err) => {
-  console.log(err);
-});
+mongoose.connect(process.env.MongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
 app.use('/home', UserRouter);
 app.use('/Admin', AdminRouter);
 app.use('/company', CompanyRouter);
 
-app.get('', (req, res, next) => {
-  try {
-    res.sendFile(path.join(__dirname, 'Frontend/app/build', 'index.html'));
-  } catch (error) {
-    next(error);
-  }
+
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend/app/build', 'index.html'));
 });
+
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
-  console.log(`port ${PORT} is connected`);
+  console.log(`Server is running on port ${PORT}`);
 });
